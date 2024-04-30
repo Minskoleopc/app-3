d92cc50343916f27710914021121ce365cc4c03a
unknown <Minskoleopc>
2024-04-30 19:51:42 +0530

    added

--- src/index.js
+++ src/index.js
@@ -1,17 +1,126 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { Component } from "react";
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+function App() {
+    return (
+        <div>
+            <h1>Hello</h1>
+            <ButtonClick></ButtonClick>
+            <PropExample name="chinmay" lastName="deshpande" />
+            <MyClassComponent></MyClassComponent>
+            <Counter></Counter>
+        </div>
+    )
+}
+
+function PropExample({ name }) {
+    return <h1>Hello my name is {name}</h1>
+}
+
+// program 3
+
+class Counter extends Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            count: 0
+        };
+    }
+
+    increment = () => {
+        // this.state.count = this.state.count + 1
+        // console.log(this.state.count)
+
+        this.setState({
+            count:this.state.count + 1
+        })
+
+    };
+
+    decrement = () => {
+        // this.state.count = this.state.count - 1
+        // console.log(this.state.count)
+        this.setState({
+            count:this.state.count - 1
+        })
+    };
+
+
+
+    render() {
+        return (
+            <>
+                <h1>Counter</h1>
+                <p>Counter: {this.state.count}</p>
+                <button onClick={this.increment}>Increment</button>
+                <button onClick={this.decrement}>Decrement</button>
+            </>
+
+        )
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Program 2 - class based component
+class MyClassComponent extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Hi This is Called Class Based Component</h1>
+            </div>
+        )
+    }
+}
+
+// program 1  ------ example of event click
+// apperance logic and data
+function ButtonClick() {
+    const handleClick = () => {
+        alert("This is example of button is clicked")
+    }
+    return (
+        <div>
+            <h1>Click example</h1>
+            <button onClick={handleClick}>Click</button>
+        </div>
+    )
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(<App />);
+
+
+
